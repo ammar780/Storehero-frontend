1761a5694f547cfef0b087b8a7cf20e21ead0ac1
ammar780 <142854970+ammar780@users.noreply.github.com>
2026-08-21 21:17:00 +0500

    Delete Storehero-frontend-main/TVS-Frontend-FIXED directory

--- Storehero-frontend-main/TVS-Frontend-FIXED/src/pages/EmailitEmailsPage.jsx
+++
@@ -1,4 +0,0 @@
-import ProviderEmailDetailPage from './EmailitDetailPage';
-export default function EmailitEmailsPage() {
-  return <ProviderEmailDetailPage provider="emailit" title="Emailit" accentColor="text-indigo-600" />;
-}
--- Storehero-frontend-main/TVS-Frontend-FIXED/src/pages/EnginemailerEmailsPage.jsx
+++
@@ -1,4 +0,0 @@
-import ProviderEmailDetailPage from './EmailitDetailPage';
-export default function EnginemailerEmailsPage() {
-  return <ProviderEmailDetailPage provider="enginemailer" title="Enginemailer" accentColor="text-orange-600" />;
-}
